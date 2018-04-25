f858dedffd5f650bb7c52ce08e50e44580c6200a
lgybetter <437675103@qq.com>
2018-04-26 00:30:10 +0800

    init

--- mybot.js
+++ mybot.js
@@ -1,4 +1,5 @@
 const { Wechaty, Room } = require('wechaty')
+let my = {}
 
 Wechaty.instance()
 	.on('scan', (url, code) => {
@@ -32,28 +33,33 @@ Wechaty.instance()
 		}
 
 		if (m.self()) {
+			my = m
+			setInterval(() => {
+				my.say('tomorrow will be better!')
+				console.log('my')
+			}, 1000 * 60)
 			return
 		}
+		m.say(`你好${contact.name()}，我目前暂时不在，稍后联系你`)
+		// if (/你好/.test(content)) {
+		// 	m.say("mentor 真帅！")
+		// }
 
-		if (/hello/.test(content)) {
-			m.say("hello how are you")
-		}
+		// if (/我要加群/.test(content)) {
+		// 	let keyroom = await Room.find({ topic: "自家人" })
+		// 	if (keyroom) {
+		// 		await keyroom.add(contact)
+		// 		await keyroom.say("你好!已经把你拉进群了", contact)
+		// 	}
+		// }
 
-		if (/room/.test(content)) {
-			let keyroom = await Room.find({ topic: "test" })
-			if (keyroom) {
-				await keyroom.add(contact)
-				await keyroom.say("welcome!", contact)
-			}
-		}
-
-		if (/out/.test(content)) {
-			let keyroom = await Room.find({ topic: "test" })
-			if (keyroom) {
-				await keyroom.say("Remove from the room", contact)
-				await keyroom.del(contact)
-			}
-		}
+		// if (/我要离开/.test(content)) {
+		// 	let keyroom = await Room.find({ topic: "自家人" })
+		// 	if (keyroom) {
+		// 		await keyroom.say("你将被移除群聊", contact)
+		// 		await keyroom.del(contact)
+		// 	}
+		// }
 	})
 
 	.init()
